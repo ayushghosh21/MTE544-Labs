62e847f09fa37de5ea98ee577f3197ac81903c2b
Nick Shaju <nickshaju@gmail.com>
2022-11-26 22:11:07 -0500

    A star implementation

--- src/mte544_a_star/src/a_star_skeleton1.py
+++ src/mte544_a_star/src/a_star_skeleton1.py
@@ -1,5 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import yaml
+from scipy.ndimage import rotate
+import heapq 
+from collections import deque as queue
+
+# List containing directions for neighboring indices (i,j)
+dirVal = [ (1,0), (0,1), (1,1), (1,-1), (-1, 1), (-1, -1), (-1,0), (0,-1)]
+
+# Function to check if a cell to is be visited or not
+
+def isLegal(maze, visited, nxt_pos):
+   
+    #Check if index lies is out of bounds
+    if (nxt_pos[0] < 0 or nxt_pos[1] < 0 or nxt_pos[0] >= maze.shape[0] or nxt_pos[1] >= maze.shape[1]):
+        return False
+        
+    #Check if index is visited
+    if (visited[nxt_pos[0],nxt_pos[1]]):
+        return False
+
+    #Check if index is the maze wall
+    if(maze[nxt_pos[0],nxt_pos[1]] == 1):
+        return False
+    return True
+
 class node():
     """A node class for A* Pathfinding"""
 
@@ -7,102 +32,94 @@ class node():
         self.parent = parent
         self.position = position
 
-        self.g = 0
-        self.h = 0
-        self.f = 0
+        self.g = 0  #Cost
+        self.h = 0  #Heuristic
+        self.f = 0  #Total
 
     def __eq__(self, other):
         return self.position == other.position
+    
+    def __lt__(self, other):
+        return self.position < other.position
+        
+def heuristic(x1,y1,x2,y2): # heuristic function based on the manhattan distance between the points x1,y1 x2,y2
+    return abs(x1-x2) + abs(y1-y2) 
 
-
-def astar(maze, start, end):
+def astar(maze: np.ndarray, start, end):
     """Returns a list of tuples as a path from the given start to the given end in the given maze"""
 
     # Create start and end node
     start_node = node(None, start)
-    start_node.g = start_node.h = start_node.f = 0
     end_node = node(None, end)
-    end_node.g = end_node.h = end_node.f = 0
 
     # Initialize both open and closed list
     open_list = []
-    closed_list = []
-
+    path = queue()
+    visited = np.full(maze.shape, False)
+    visited[start_node.position[0],start_node.position[1]] = True
     # Add the start node
-    open_list.append(start_node)
-
+    heapq.heappush(open_list, (0 ,(start_node, path))) # Priority queue using heappush, adding the starting node 
+    
     # Loop until you find the end node
+
     while len(open_list) > 0:
 
-        # Get the current node
-        current_node = open_list[0]
-        current_index = 0
-        for index, item in enumerate(open_list):
-            if item.f < current_node.f:
-                current_node = item
-                current_index = index
-
-        # Pop current off open list, add to closed list
-        open_list.pop(current_index)
-        closed_list.append(current_node)
-
+        # Get t he current node
+        item = heapq.heappop(open_list) 
+        current_node = item[1][0]
+        cost = current_node.g + 1
+        p = item[1][1].copy()
+        p.append(current_node.position)
         # Found the goal, you can also implement what should happen if there is no possible path
         if current_node == end_node:
             # Complete here code to return the shortest path found
-            path = []
-            return path
+            return p
 
         # Complete here code to generate children, which are the neighboring nodes. You should use 4 or 8 points connectivity for a grid.
-        children = []
 
-        # Loop through children to update the costs
-        for child in children:
-            # Child is on the closed list
-            for closed_child in closed_list:
-                if child == closed_child:
-                    break
-            else:
-                # Create the f, g, and h values, replace the 0s with appropriate formulations of the costs
-                child.g = 0
-                child.h = 0
-                child.f = 0
-
-                # Complete here code to check whether to add a child to the open list
-                open_list.append(child)
+        for adj in dirVal:
+            nxt_pos = tuple(np.add(current_node.position ,adj))
+            if (isLegal(maze, visited, nxt_pos)):
+                est_cost = cost + 1 + heuristic(nxt_pos[0], nxt_pos[1], end_node.position[0], end_node.position[0]) #estimated cost based on heuristic
+                nxt_node = node(None, nxt_pos)
+                nxt_node.g = cost
+                heapq.heappush(open_list, (est_cost, (nxt_node, p)))
+                visited[nxt_pos[0],nxt_pos[1]] = True
+        
 
 def main():
 
-    # Load your maze here
-    maze = []
-    
-    # This is an example maze you can use for testing, replace it with loading the actual map
-    maze = [[0,   0,   0,   0,   1,   0, 0, 0, 0, 0],
-            [0, 0.8,   1,   0,   1,   0, 0, 0, 0, 0],
-            [0, 0.9,   1,   0,   1,   0, 1, 0, 0, 0],
-            [0,   1,   0,   0,   1,   0, 1, 0, 0, 0],
-            [0,   1,   0,   0,   1,   0, 0, 0, 0, 0],
-            [0,   0,   0, 0.9,   0,   1, 0, 0, 0, 0],
-            [0,   0, 0.9,   1,   1, 0.7, 0, 0, 0, 0],
-            [0,   0,   0,   1,   0,   0, 0, 0, 0, 0],
-            [0,   0,   0,   0, 0.9,   0, 0, 0, 0, 0],
-            [0,   0,   0,   0,   0,   0, 0, 0, 0, 0]]
+    map_file = "Office_Map/map.pgm"
 
-    
-    
+    with open(map_file, 'rb') as pgmf:
+        im = plt.imread(pgmf)
+
+    # Inverting obstacle values from 0 to 1 
+    maze = ~im.astype(bool)
+
+    yaml_file = "Office_Map/map.yaml"
+
+    with open(yaml_file, 'r') as stream:
+        data_loaded = yaml.safe_load(stream)
+
+    # Store map parameters
+    origin = data_loaded['origin']
+    map_res = data_loaded['resolution']
+
     # Define here your start and end points
-    start = (0, 0)
-    end = (6, 6)
+    start = (56, 170)
+    end = (35, 100)
     
     # Compute the path with your implementation of Astar
-    path = np.asarray( astar(maze, start, end), dtype=np.float)
+    path = np.array(list(astar(maze, start, end)))
     maze_plot=np.transpose(np.nonzero(maze))
 
-    plt.plot(maze_plot[:,0], maze_plot[:,1], 'o')
+    plt.plot(maze_plot[:,0], maze_plot[:,1], '.')
     
     if not np.any(path): # If path is empty, will be NaN, check if path is NaN
         print("No path found")
     else:
-        plt.plot(path[:,0], path[:,1])
+        plt.plot(path[:,0], path[:,1],linewidth=3)
     plt.grid()
     plt.show()
 
