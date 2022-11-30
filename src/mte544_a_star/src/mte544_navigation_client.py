e74fdd80a5eb5c6f94bf2aadc61d326e9723d144
Ayush Ghosh <ayushghosh21@gmail.com>
2022-11-30 01:57:05 -0500

    Added cmd line option to define goal. Server now use current pose as initial pose. Completed feedback results for action client.

--- src/mte544_a_star/src/mte544_navigation_client.py
+++ src/mte544_a_star/src/mte544_navigation_client.py
@@ -6,29 +6,83 @@ from rclpy.node import Node
 from rclpy.qos import ReliabilityPolicy, QoSProfile
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from geometry_msgs.msg import PoseStamped
+from builtin_interfaces.msg import Duration
+from tf2_ros import TransformException
+from tf2_ros.buffer import Buffer
+from tf2_ros.transform_listener import TransformListener
+
 
 from mte544_action_interfaces.action import Move2Goal
 
 class AStarClient(Node):
 
     def __init__(self):
-        super().__init__('a_star_action_client')
+        super().__init__('a_star_client')
         self._action_client = ActionClient(self, Move2Goal, 'mte_544_a_star')
-        self.initial_pose_sub = self.create_subscription(
-            PoseWithCovarianceStamped, '/initialpose', self.initial_pose_callback, QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
+        # self.initial_pose_sub = self.create_subscription(
+        #     PoseWithCovarianceStamped, '/initialpose', self.initial_pose_rviz_callback, QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
         self.goal_pose_sub = self.create_subscription(
-            PoseStamped, '/goal_pose', self.goal_pose_callback, QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
+            PoseStamped, '/goal_pose', self.goal_pose_rviz_callback, QoSProfile(depth=10, reliability=ReliabilityPolicy.BEST_EFFORT))
+        self.tf_buffer = Buffer()
+        self.tf_listener = TransformListener(self.tf_buffer, self)
 
         self.initial_pose = None
         self.goal_pose = None
 
-    def initial_pose_callback(self, msg):
-        if self.initial_pose is None:
-            self.initial_pose = msg
-            self.get_logger().info(f"Initialize position: {self.initial_pose.pose.pose.position.x, self.initial_pose.pose.pose.position.y}")
+        self.declare_parameter('predefined_goal', False)
+        self.declare_parameter('goal_x', 0.0)
+        self.declare_parameter('goal_y', 0.0)
+
+    def get_current_pose(self):
+        """Get current pose of Turtlebot. Find the transform between base_footprint and map"""
+
+        from_frame_rel = 'base_footprint'
+        to_frame_rel = 'map'
+
+        for i in range(500):
+            rclpy.spin_once(self)
+            try:
+                t = self.tf_buffer.lookup_transform(
+                    to_frame_rel,
+                    from_frame_rel,
+                    rclpy.time.Time())
+                
+                curr_pose_x = t.transform.translation.x
+                curr_pose_y = t.transform.translation.y
+                base_map_rot = t.transform.rotation
+            
+                quaternion = (
+                    base_map_rot.x,
+                    base_map_rot.y,
+                    base_map_rot.z,
+                    base_map_rot.w
+                )
+
+                initial_pose = PoseWithCovarianceStamped()
+
+                initial_pose.pose.pose.position.x = curr_pose_x
+                initial_pose.pose.pose.position.y = curr_pose_y
+                initial_pose.pose.pose.orientation.x = quaternion[0]
+                initial_pose.pose.pose.orientation.y = quaternion[1]
+                initial_pose.pose.pose.orientation.z = quaternion[2]
+                initial_pose.pose.pose.orientation.w = quaternion[3]
+
+                return initial_pose
+
+            except TransformException as ex:
+                # self.get_logger().info(
+                #     f'Could not transform {to_frame_rel} to {from_frame_rel}: {ex}')
+                pass
+                #return None
+    
+
+    # def initial_pose_rviz_callback(self, msg):
+    #     if self.initial_pose is None:
+    #         self.initial_pose = msg
+    #         self.get_logger().info(f"Initialize position: {self.initial_pose.pose.pose.position.x, self.initial_pose.pose.pose.position.y}")
             #print(self.initial_pose)
 
-    def goal_pose_callback(self, msg):
+    def goal_pose_rviz_callback(self, msg):
         if self.goal_pose is None:
             self.goal_pose = msg
             self.get_logger().info(f"Goal position: {self.goal_pose.pose.position.x, self.goal_pose.pose.position.y}")
@@ -42,6 +96,7 @@ class AStarClient(Node):
         if not (initial_x and initial_y and goal_x and goal_y):
             while True:
                 if self.initial_pose is None:
+                    self.initial_pose = self.get_current_pose()
                     rclpy.spin_once(self)
                 else:
                     break
@@ -63,7 +118,7 @@ class AStarClient(Node):
             goal_msg.goal_y = goal_y
         
         self._action_client.wait_for_server()
-        self._send_goal_future = self._action_client.send_goal_async(goal_msg)
+        self._send_goal_future = self._action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
         self._send_goal_future.add_done_callback(self.goal_response_callback)
 
     def goal_response_callback(self, future):
@@ -84,18 +139,33 @@ class AStarClient(Node):
         self.goal_pose = None
         rclpy.shutdown()
 
+    def feedback_callback(self, feedback_msg):
+        feedback = feedback_msg.feedback
+        curr_pose_x = feedback.current_pose.pose.position.x
+        curr_pose_y = feedback.current_pose.pose.position.y
+        dist = feedback.distance_remaining
+        elapsed_duration = feedback.navigation_time
+        elapsed_time = elapsed_duration.sec + elapsed_duration.nanosec/1.0E9
+        self.get_logger().info(f"Feedback: Robot Pose [{curr_pose_x:.3f},{curr_pose_y:.3f}], Elapsed Time [{elapsed_time:.3f}], Distance [{dist:.3f}]")
 
 def main(args=None):
     rclpy.init(args=args)
 
     action_client = AStarClient()
+    
+    goal_x = action_client.get_parameter('goal_x').value
+    goal_y = action_client.get_parameter('goal_y').value
+    initial_pose = None
 
-    initial_x = 0
-    initial_y = 0
-    goal_x = 10 
-    goal_y = 10
-    action_client.send_goal()
-
+    if action_client.get_parameter('predefined_goal'):
+        initial_pose = action_client.get_current_pose()
+    
+    if initial_pose is not None:
+        initial_x = initial_pose.pose.pose.position.x
+        initial_y = initial_pose.pose.pose.position.y
+        action_client.send_goal(initial_x, initial_y, goal_x, goal_y)
+    else:
+        action_client.send_goal()
     rclpy.spin(action_client)
 
 
